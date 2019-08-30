a10b6a4052eb2618b6bb566c7acbffcfd22ece0f
FynnKaeser <fynn.kaeser@icloud.com>
2019-08-30 10:29:59 +0200

    Added new tests
    * Calculator Sum method test
    * Dictionary duplicate test
    * Dictionary adding elements test

---
+++ src/biz.dfch.CS.Playground.Fynn.Tests/20190830/CalculatorTest.cs
@@ -0,0 +1,38 @@
+﻿/**
+ * Copyright 2019 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using biz.dfch.CS.Playground.Fynn._20190830;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.Playground.Fynn.Tests._20190830
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void SumOfTwoAndTwoReturnsFour()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Sum(2, 2);
+
+            // Assert
+            Assert.AreEqual(4, result);
+        }
+    }
+}
---
+++ src/biz.dfch.CS.Playground.Fynn.Tests/20190830/DictionaryTest.cs
@@ -0,0 +1,63 @@
+﻿/**
+ * Copyright 2019 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.Playground.Fynn.Tests._20190830
+{
+    [TestClass]
+    public class DictionaryTest
+    {
+        [TestMethod]
+        public void AddingTwoDifferentElementsToDictionarySucceeds()
+        {
+            // Arrange
+            var dictionary = new Dictionary<int, string>();
+            var value1 = "Hello";
+            var value2 = "World";
+
+            // Act
+            dictionary.Add(1, value1);
+            dictionary.Add(2, value2); 
+
+            // Assert
+            Assert.AreEqual(2, dictionary.Count);
+            Assert.IsTrue(dictionary.ContainsKey(1));
+            Assert.AreEqual(value1, dictionary[1]);
+            Assert.IsTrue(dictionary.ContainsKey(2));
+            Assert.AreEqual(value2, dictionary[2]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddingDuplicateElementToDictionaryThrowsArgumentException()
+        {
+            // Arrange
+            var dictionary = new Dictionary<int, string>();
+            var value1 = "Hello";
+            var value2 = "World";
+
+            // Act
+            dictionary.Add(1, value1);
+            dictionary.Add(1, value2);
+
+            // Assert
+
+        }
+    }
+}
