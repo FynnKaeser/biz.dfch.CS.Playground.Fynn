35787160e8636094ffc66b4337657390b8a82b8e
FynnKaeser <fynn.kaeser@icloud.com>
2019-10-21 14:28:15 +0200

    * Item 43 First/Single * Item 43 UnitTest

---
+++ src/biz.dfch.CS.Playground.Fynn.Tests/20191021/Item43Test.cs
@@ -0,0 +1,153 @@
+﻿/**
+ * Copyright 2019 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using biz.dfch.CS.Playground.Fynn._20191021;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.Playground.Fynn.Tests._20191021
+{
+    [TestClass]
+    public class Item43Test
+    {
+        [TestMethod]
+        public void GetFirstPlayerIsEqualToExpected()
+        {
+            // Arrange
+            var sut = new Item43();
+            var expected = sut.CreatePlayer("Fynn", "Kaeser", 10);
+            var meier = sut.CreatePlayer("Fynn", "Meier", 10);
+
+            // Act
+            var result = sut.GetFirstPlayer(firstName: "Fynn");
+
+            // Assert
+            Assert.AreEqual(result.LastName, expected.LastName);
+        }
+
+        [ExpectedException(typeof(InvalidOperationException))]
+        [TestMethod]
+        public void GetFirstPlayerThrowsExceptionIfNoPlayerFound()
+        {
+            // Arrange
+            var sut = new Item43();
+            var kaeser = sut.CreatePlayer("Fynn", "Kaeser", 10);
+            var meier = sut.CreatePlayer("Fynn", "Meier", 10);
+
+            // Act
+            var x = sut.GetFirstPlayer(firstName: "Kevin");
+
+            // Assert
+            // Intentionally nothing
+        }
+
+        [ExpectedException(typeof(InvalidOperationException))]
+        [TestMethod]
+        public void GetSinglePlayerThrowsExceptionIfMultipleResultWereFound()
+        {
+            // Arrange
+            var sut = new Item43();
+            var kaeser = sut.CreatePlayer("Fynn", "Kaeser", 10);
+            var meier = sut.CreatePlayer("Fynn", "Meier", 10);
+
+            // Act
+            var x = sut.GetSinglePlayer(firstName: "Fynn");
+
+            // Assert
+            // Intentionally nothing
+        }
+
+        [ExpectedException(typeof(InvalidOperationException))]
+        [TestMethod]
+        public void GetSinglePlayerThrowsExceptionIfNoResultWereFound()
+        {
+            // Arrange
+            var sut = new Item43();
+            var kaeser = sut.CreatePlayer("Fynn", "Kaeser", 10);
+            var meier = sut.CreatePlayer("Fynn", "Meier", 10);
+
+            // Act
+            var x = sut.GetSinglePlayer(firstName: "Kevin");
+
+            // Assert
+            // Intentionally nothing
+        }
+
+        [TestMethod]
+        public void GetFirstOrDefaultWhenNoMatchReturnsNull()
+        {
+            // Arrange
+            var sut = new Item43();
+            var kaeser = sut.CreatePlayer("Fynn", "Kaeser", 10);
+            var meier = sut.CreatePlayer("Fynn", "Meier", 10);
+
+            // Act
+            var result = sut.GetFirstOrDefaultPlayer(firstName: "Kevin");
+
+            // Assert
+            Assert.AreEqual(result, null);
+        }
+
+        [TestMethod]
+        public void GetFirstOrDefaultWhenMultipleReturnsExpected()
+        {
+            // Arrange
+            var sut = new Item43();
+            var expected = sut.CreatePlayer("Fynn", "Kaeser", 10);
+            var meier = sut.CreatePlayer("Fynn", "Meier", 10);
+
+            // Act
+            var result = sut.GetFirstOrDefaultPlayer(firstName: "Fynn");
+
+            // Assert
+            Assert.AreEqual(result.LastName, expected.LastName);
+        }
+
+
+        [TestMethod]
+        public void GetSingleOrDefaultWhenNoMatchReturnsNull()
+        {
+            // Arrange
+            var sut = new Item43();
+            var kaeser = sut.CreatePlayer("Fynn", "Kaeser", 10);
+            var meier = sut.CreatePlayer("Fynn", "Meier", 10);
+
+            // Act
+            var result = sut.GetSingleOrDefaultPlayer(firstName: "Kevin");
+
+            // Assert
+            Assert.AreEqual(result, null);
+        }
+
+        [ExpectedException(typeof(InvalidOperationException))]
+        [TestMethod]
+        public void GetSingleOrDefaultWhenMultipleThrowsException()
+        {
+            // Arrange
+            var sut = new Item43();
+            var kaeser = sut.CreatePlayer("Fynn", "Kaeser", 10);
+            var meier = sut.CreatePlayer("Fynn", "Meier", 10);
+
+            // Act
+            var result = sut.GetSingleOrDefaultPlayer(firstName: "Fynn");
+
+            // Assert
+            // Intentionally nothing
+        }
+
+
+    }
+}
---
+++ src/biz.dfch.CS.Playground.Fynn/20191021/Item43.cs
@@ -0,0 +1,120 @@
+﻿/**
+ * Copyright 2019 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biz.dfch.CS.Playground.Fynn._20191021
+{
+    public class Item43
+    {
+        private IList<Player> players = new List<Player>();
+
+        public IList<Player> GetPlayers()
+        {
+            return players;
+        }
+
+        public Player CreatePlayer(string firstName, string lastName, int goalScored)
+        {
+            var player = new Player(firstName, lastName, goalScored);
+            players.Add(player);
+
+            return player;
+        }
+
+        public Player GetFirstPlayer(string firstName)
+        {
+            try
+            {
+                var player = (from p in players
+                              where p.FirstName == firstName
+                              select p).First();
+
+                return player;
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new InvalidOperationException(e.Message);
+            }
+        }
+
+        public Player GetSinglePlayer(string firstName)
+        {
+            try
+            {
+                var player = (from p in GetPlayers()
+                              where p.FirstName == firstName
+                              select p).Single();
+
+                return player;
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new InvalidOperationException(e.Message);
+            }
+        }
+
+        public Player GetFirstOrDefaultPlayer(string firstName)
+        {
+            try
+            {
+                var player = (from p in players
+                              where p.FirstName == firstName
+                              select p).FirstOrDefault();
+
+                return player;
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new InvalidOperationException(e.Message);
+            }
+        }
+
+        public Player GetSingleOrDefaultPlayer(string firstName)
+        {
+            try
+            {
+                var player = (from p in GetPlayers()
+                              where p.FirstName == firstName
+                              select p).SingleOrDefault();
+
+                return player;
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new InvalidOperationException(e.Message);
+            }
+        }
+    }
+
+    public class Player
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int GoalsScored { get; set; }
+
+        public Player(string firstName, string lastName, int goalsScored)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            GoalsScored = goalsScored;
+        }
+    }
+}
