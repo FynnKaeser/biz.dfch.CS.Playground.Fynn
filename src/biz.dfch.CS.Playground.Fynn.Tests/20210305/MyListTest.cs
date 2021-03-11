c5a2d920038684acae5cb41ab1954ae3696f04dc
FynnKaeser <fynn.kaeser@icloud.com>
2021-03-11 12:47:54 +0100

    Adding Functionality to MyList - DeleteAt - Delete - Delete list - Move element ot position - Define size of list when creating - Switch position of elements - Count elements
    Added tests
    
    #3

--- src/biz.dfch.CS.Playground.Fynn.Tests/20210305/MyListTest.cs
+++ src/biz.dfch.CS.Playground.Fynn.Tests/20210305/MyListTest.cs
@@ -15,7 +15,6 @@
  */
 
 using System;
-using System.IO;
 using biz.dfch.CS.Playground.Fynn._20210305;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -28,7 +27,7 @@ namespace biz.dfch.CS.Playground.Fynn.Tests._20210305
         public void SearchForElementInListReturnsElement()
         {
             // Arrange
-            var sut = new MyList<string>(2);
+            var sut = new MyList<string>(3);
             sut.Add("World");
             sut.Add("Helmet");
             sut.Add("Breakpoint");
@@ -37,14 +36,14 @@ namespace biz.dfch.CS.Playground.Fynn.Tests._20210305
             var result = sut.Search("World");
 
             // Assert
-            Assert.AreEqual("World", result);
+            Assert.AreEqual(0, result);
         }
 
         [TestMethod]
-        public void SearchForElementWhichDoesNotExistReturnsDefault()
+        public void SearchForElementWhichDoesNotExistReturnsMinusOne()
         {
             // Arrange
-            var sut = new MyList<string>(2);
+            var sut = new MyList<string>(3);
             sut.Add("World");
             sut.Add("Helmet");
             sut.Add("Breakpoint");
@@ -53,7 +52,7 @@ namespace biz.dfch.CS.Playground.Fynn.Tests._20210305
             var result = sut.Search("Console");
 
             // Assert
-            Assert.AreEqual(default(string), result);
+            Assert.AreEqual(-1, result);
         }
 
         [TestMethod]
@@ -61,210 +60,360 @@ namespace biz.dfch.CS.Playground.Fynn.Tests._20210305
         {
             // Arrange
             var sut = new MyList<string>(4);
-        
+            var expectedCount = 4;
+            var expectedIndexElementOne = 0;
+            var expectedIndexElementTwo = 1;
+            var expectedIndexElementThree = 2;
+            var expectedIndexElementFour = 3;
+
             // Act
             sut.Add("1");
             sut.Add("2");
             sut.Add("3");
             sut.Add("4");
 
-            var start = sut.Start;
-            var end = sut.End;
+            var resultCount = sut.Count();
+            var resultIndexElementOne = sut.Search("1");
+            var resultIndexElementTwo = sut.Search("2");
+            var resultIndexElementThree = sut.Search("3");
+            var resultIndexElementFour = sut.Search("4");
 
             // Assert
-            Assert.AreEqual("1", start.Value);
-            Assert.AreEqual("2", start.Next.Value);
-            Assert.AreEqual("3", start.Next.Next.Value);
-            Assert.AreEqual("4", start.Next.Next.Next.Value);
-            
-            Assert.AreEqual("4", end.Value);
-            Assert.AreEqual("3", end.Previous.Value);
-            Assert.AreEqual("2", end.Previous.Previous.Value);
-            Assert.AreEqual("1", end.Previous.Previous.Previous.Value);
+            Assert.AreEqual(expectedCount, resultCount);
+            Assert.AreEqual(expectedIndexElementOne, resultIndexElementOne);
+            Assert.AreEqual(expectedIndexElementTwo, resultIndexElementTwo);
+            Assert.AreEqual(expectedIndexElementThree, resultIndexElementThree);
+            Assert.AreEqual(expectedIndexElementFour, resultIndexElementFour);
+
         }
         
         [TestMethod]
         public void AddingElementAtPositionThreeSucceeds()
         {
             // Arrange
-            var sut = new MyList<string>(100);
+            var sut = new MyList<string>(4);
             sut.Add("Hello");
             sut.Add("My");
             sut.Add("World");
+
+            var expectedIndexAddAtElement = 2;
+            var expectedIndexWorldElement = 3;
 
             // Act
             sut.AddAt(2, "AddAt");
 
-            var start = sut.Start;
-            var end = sut.End;
+            var resultIndexAddAtElement = sut.Search("AddAt");
+            var resultIndexWorldElement = sut.Search("World");
 
             // Assert
-            Assert.AreEqual("Hello", start.Value);
-            Assert.AreEqual("My", start.Next.Value);
-            Assert.AreEqual("AddAt", start.Next.Next.Value);
-            Assert.AreEqual("World", start.Next.Next.Next.Value);
-
-            Assert.AreEqual("World", end.Value);
-            Assert.AreEqual("AddAt", end.Previous.Value);
-            Assert.AreEqual("My", end.Previous.Previous.Value);
-            Assert.AreEqual("Hello", end.Previous.Previous.Previous.Value);
+            Assert.AreEqual(expectedIndexAddAtElement, resultIndexAddAtElement);
+            Assert.AreEqual(expectedIndexWorldElement, resultIndexWorldElement);
         }
 
         [TestMethod]
-        public void AddingElementAtPositionFourWhenListIsThreeElementSucceeds()
+        public void AddingElementAtEndPositionSucceeds()
         {
             // Arrange
-            var sut = new MyList<string>(100);
+            var sut = new MyList<string>(4);
             sut.Add("Hello");
             sut.Add("My");
             sut.Add("World");
+
+            var expectedIndexAddAtElement = 3;
 
             // Act
             sut.AddAt(3, "AddAt");
-
-            var start = sut.Start;
-            var end = sut.End;
+            var resultIndexAddAtElement = sut.Search("AddAt");
 
             // Assert
-            Assert.AreEqual("Hello", start.Value);
-            Assert.AreEqual("My", start.Next.Value);
-            Assert.AreEqual("World", start.Next.Next.Value);
-            Assert.AreEqual("AddAt", start.Next.Next.Next.Value);
-
-            Assert.AreEqual("AddAt", end.Value);
-            Assert.AreEqual("World", end.Previous.Value);
-            Assert.AreEqual("My", end.Previous.Previous.Value);
-            Assert.AreEqual("Hello", end.Previous.Previous.Previous.Value);
+            Assert.AreEqual(expectedIndexAddAtElement, resultIndexAddAtElement);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void AddingElementAtPositionSixWhenListIsOnlyThreeElementsLongThrowsArgumentOutOfRangeException()
+        public void AddingElementAtStartPositionSucceeds()
         {
             // Arrange
-            var sut = new MyList<string>(100);
+            var sut = new MyList<string>(3);
+            sut.Add("Hello");
+            sut.Add("My");
+
+            var expectedIndexAddAtElement = 0;
+            var expectedIndexHelloElement = 1;
+            var expectedIndexMyElement = 2;
+
+            // Act
+            sut.AddAt(0, "AddAt");
+
+            var resultIndexAddAtElement = sut.Search("AddAt");
+            var resultIndexHelloElement = sut.Search("Hello");
+            var resultIndexMyElement = sut.Search("My");
+
+            // Assert
+            Assert.AreEqual(expectedIndexAddAtElement, resultIndexAddAtElement);
+            Assert.AreEqual(expectedIndexHelloElement, resultIndexHelloElement);
+            Assert.AreEqual(expectedIndexMyElement, resultIndexMyElement);
+        }
+
+        [TestMethod]
+        [DataRow(-1)]
+        [DataRow(-42)]
+        [DataRow(4)]
+        [DataRow(42)]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void AddingElementOutsideIndexRangeThrowsIndexOutOfRangeException(int index)
+        {
+            // Arrange
+            var sut = new MyList<string>(4);
             sut.Add("Hello");
             sut.Add("My");
             sut.Add("World");
 
             // Act
-            sut.AddAt(6, "AddAt");
+            sut.AddAt(index, "AddAt");
 
             // Assert
         }
-        //
-        //[TestMethod]
-        //public void DeletingElementAtPositionThreeFromListSucceeds()
-        //{
-        //    // Arrange
-        //    var sut = new MyList<string>(3)
-        //    {
-        //        "Hello",
-        //        "My",
-        //        "World"
-        //    };
-        //
-        //    // Act
-        //    sut.DeleteAt(3);
-        //
-        //    // Assert
-        //    Assert.AreEqual(2, sut.Count);
-        //
-        //}
-        //
-        //[TestMethod]
-        //public void DeletingElementFromListSucceeds()
-        //{
-        //    // Arrange
-        //    var sut = new MyList<string>(3)
-        //    {
-        //        "Hello",
-        //        "My",
-        //        "World"
-        //    };
-        //
-        //    // Act
-        //    sut.Delete("World");
-        //
-        //    // Assert
-        //    Assert.AreEqual(2, sut.Count);
-        //}
-        //
-        //[TestMethod]
-        //public void DeletingListSucceeds()
-        //{
-        //    // Arrange
-        //    var sut = new MyList<string>(3)
-        //    {
-        //        "Hello",
-        //        "My",
-        //        "World"
-        //    };
-        //
-        //    // Act
-        //    sut.DeleteList();
-        //
-        //    // Assert
-        //    Assert.AreEqual(null, sut);
-        //}
-        //
-        //[TestMethod]
-        //public void MovingElementFromPositionOneToPositionThreeSucceeds()
-        //{
-        //    // Arrange
-        //    var sut = new MyList<string>(3)
-        //    {
-        //        "World",
-        //        "Hello",
-        //        "My"
-        //    };
-        //
-        //    // Act
-        //    sut.Move("World", 3);
-        //
-        //    // Assert
-        //    Assert.AreEqual("Hello", sut[1]);
-        //    Assert.AreEqual("My", sut[2]);
-        //    Assert.AreEqual("World", sut[3]);
-        //}
-        //
-        //[TestMethod]
-        //public void SwitchElementAtPositionOneWithElementAtPositionThreeSucceeds()
-        //{
-        //    // Arrange
-        //    var sut = new MyList<string>(3)
-        //    {
-        //        "World",
-        //        "My",
-        //        "Hello"
-        //    };
-        //
-        //    // Act
-        //    sut.Swap(1, 3);
-        //
-        //    // Assert
-        //    Assert.AreEqual("Hello", sut[1]);
-        //    Assert.AreEqual("My", sut[2]);
-        //    Assert.AreEqual("World", sut[3]);
-        //}
-        //
-        //[TestMethod]
-        //[ExpectedException(typeof(Exception))]
-        //public void AddingFifthElementToListWithCapacityOfFourThrowsException()
-        //{
-        //    // Arrange
-        //    var sut = new MyList<string>(4)
-        //    {
-        //        "Hello",
-        //        "My",
-        //        "World",
-        //        "!"
-        //    };
-        //
-        //    // Act
-        //    sut.Add("Bad");
-        //
-        //    // Assert
-        //}
+        
+        [TestMethod]
+        public void DeletingElementAtPositionThreeFromListSucceeds()
+        {
+            // Arrange
+            var sut = new MyList<string>(5);
+            sut.Add("Hello");
+            sut.Add("My");
+            sut.Add("F5");
+            sut.Add("ENG");
+            sut.Add("World");
+
+            var expectedListCount = 4;
+            var expectedIndexEngElement = 2;
+
+            // Act
+            sut.DeleteAt(2);
+            var resultIndexEngElement = sut.Search("ENG");
+
+            var resultListCount = sut.Count();
+
+            // Assert
+            Assert.AreEqual(expectedListCount, resultListCount);
+            Assert.AreEqual(expectedIndexEngElement, resultIndexEngElement);
+        }
+        
+        [TestMethod]
+        public void DeletingElementAtStartPositionSucceeds()
+        {
+            // Arrange
+            var sut = new MyList<string>(5);
+            sut.Add("Hello");
+            sut.Add("My");
+            sut.Add("F5");
+            sut.Add("ENG");
+            sut.Add("World");
+
+            var expectedListCount = 4;
+            var expectedIndexMyElement = 0;
+
+            // Act
+            sut.DeleteAt(0);
+            var resultIndexMyElement = sut.Search("My");
+
+            var resultListCount = sut.Count();
+
+            // Assert
+            Assert.AreEqual(expectedListCount, resultListCount);
+            Assert.AreEqual(expectedIndexMyElement, resultIndexMyElement);
+        }
+        
+        [TestMethod]
+        public void DeletingElementAtEndPositionSucceeds()
+        {
+            // Arrange
+            var sut = new MyList<string>(5);
+            sut.Add("Hello");
+            sut.Add("My");
+            sut.Add("F5");
+            sut.Add("ENG");
+            sut.Add("World");
+
+            var expectedListCount = 4;
+            var expectedIndexWorldElement = -1;
+
+            // Act
+            sut.DeleteAt(4);
+            var resultIndexWorldElement = sut.Search("World");
+
+            var resultListCount = sut.Count();
+
+            // Assert
+            Assert.AreEqual(expectedListCount, resultListCount);
+            Assert.AreEqual(expectedIndexWorldElement, resultIndexWorldElement);
+        }
+
+        [TestMethod]
+        public void DeletingElementFromListSucceeds()
+        {
+            // Arrange
+            var sut = new MyList<string>(5);
+            sut.Add("Hello");
+            sut.Add("My");
+            sut.Add("F5");
+            sut.Add("ENG");
+            sut.Add("World");
+
+            var expectedListCount = 4;
+            var expectedIndexEngElement = 2;
+            var expectedIndexF5Element = -1;
+
+            // Act
+            sut.Delete("F5");
+
+            var resultListCount = sut.Count();
+            var resultIndexEngElement = sut.Search("Eng");
+            var resultIndexF5Element = sut.Search("F5");
+
+            // Assert
+            Assert.AreEqual(expectedListCount, resultListCount);
+            Assert.AreEqual(expectedIndexEngElement, resultIndexEngElement);
+            Assert.AreEqual(expectedIndexF5Element, resultIndexF5Element);
+        }
+
+        [TestMethod]
+        public void DeletingStartElementFromListSucceeds()
+        {
+            // Arrange
+            var sut = new MyList<string>(5);
+            sut.Add("Hello");
+            sut.Add("My");
+            sut.Add("F5");
+            sut.Add("ENG");
+            sut.Add("World");
+
+            var expectedListCount = 4;
+            var expectedIndexMyElement = 0;
+            var expectedIndexHelloElement = -1;
+
+            // Act
+            sut.Delete("F5");
+
+            var resultListCount = sut.Count();
+            var resultIndexMyElement = sut.Search("My");
+            var resultIndexHelloElement = sut.Search("Hello");
+
+            // Assert
+            Assert.AreEqual(expectedListCount, resultListCount);
+            Assert.AreEqual(expectedIndexMyElement, resultIndexMyElement);
+            Assert.AreEqual(expectedIndexHelloElement, resultIndexHelloElement);
+        }
+
+        [TestMethod]
+        public void DeletingListSucceeds()
+        {
+            // Arrange
+            var sut = new MyList<string>(3);
+            sut.Add("Hello");
+            sut.Add("My");
+            sut.Add("F5");
+
+            // Act
+            sut.DeleteList();
+        
+            // Assert
+            Assert.AreEqual(null, sut);
+        }
+        
+        [TestMethod]
+        public void MovingStartElementToIndexThreeSucceeds()
+        {
+            // Arrange
+            var sut = new MyList<string>(5);
+            sut.Add("Hello");
+            sut.Add("My");
+            sut.Add("F5");
+            sut.Add("ENG");
+            sut.Add("World");
+
+            var expectedIndexMovedElement = 3;
+            var expectedIndexMyElement = 0;
+
+            // Act
+            sut.Move("Hello", 3);
+
+            var resultIndexMovedElement = sut.Search("Hello");
+            var resultIndexMyElement = sut.Search("My");
+
+            // Assert
+            Assert.AreEqual(expectedIndexMovedElement, resultIndexMovedElement);
+            Assert.AreEqual(expectedIndexMyElement, resultIndexMyElement);
+        }
+        
+        [TestMethod]
+        public void MovingElementToStartIndexSucceeds()
+        {
+            // Arrange
+            var sut = new MyList<string>(5);
+            sut.Add("Hello");
+            sut.Add("My");
+            sut.Add("F5");
+            sut.Add("ENG");
+            sut.Add("World");
+
+            var expectedIndexMovedElement = 0;
+            var expectedIndexHelloElement = 1;
+            var expectedIndexMyElement = 2;
+
+            // Act
+            sut.Move("F5", 0);
+
+            var resultIndexMovedElement = sut.Search("F5");
+            var resultIndexHelloElement = sut.Search("Hello");
+            var resultIndexMyElement = sut.Search("My");
+
+            // Assert
+            Assert.AreEqual(expectedIndexMovedElement, resultIndexMovedElement);
+            Assert.AreEqual(expectedIndexHelloElement, resultIndexHelloElement);
+            Assert.AreEqual(expectedIndexMyElement, resultIndexMyElement);
+        }
+
+        [TestMethod]
+        public void SwitchingStartElementWithElementAtIndexThreeSucceeds()
+        {
+            // Arrange
+            var sut = new MyList<string>(5);
+            sut.Add("Hello");
+            sut.Add("My");
+            sut.Add("F5");
+            sut.Add("ENG");
+            sut.Add("World");
+
+            var expectedIndexHelloElement = 3;
+            var expectedIndexEngElement = 0;
+
+            // Act
+            sut.Swap(0, 3);
+
+            var resultIndexHelloElement = sut.Search("Hello");
+            var resultIndexEngElement= sut.Search("ENG");
+
+            // Assert
+            Assert.AreEqual(expectedIndexHelloElement, resultIndexHelloElement);
+            Assert.AreEqual(expectedIndexEngElement, resultIndexEngElement);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void AddingFifthElementToListWithCapacityOfFourThrowsException()
+        {
+            // Arrange
+            var sut = new MyList<string>(4);
+            sut.Add("Hello");
+            sut.Add("My");
+            sut.Add("F5");
+            sut.Add("ENG"); ;
+        
+            // Act
+            sut.Add("Bad");
+        
+            // Assert
+        }
     }  
 }
--- src/biz.dfch.CS.Playground.Fynn/20210305/MyList.cs
+++ src/biz.dfch.CS.Playground.Fynn/20210305/MyList.cs
@@ -20,114 +20,271 @@ namespace biz.dfch.CS.Playground.Fynn._20210305
 {
     public class MyList<TItem>
     {
-        public MyListElement<TItem> Start { private set; get; }
-        public MyListElement<TItem> End { private set; get; }
+        private readonly int capacity;
+        private MyListElement<TItem> start;
+        private MyListElement<TItem> end;
 
         public MyList(int capacity)
         {
+            this.capacity = capacity;
         }
 
-        public TItem Search(TItem item)
+        public int Search(TItem item)
         {
-            var tempElement = Start;
+            var tempElement = start;
+            var elementIndex = 0;
 
             while (null != tempElement)
             {
                 if (item.Equals(tempElement.Value))
                 {
-                    return tempElement.Value;
+                    return elementIndex;
                 }
 
                 tempElement = tempElement.Next;
+                elementIndex++;
             }
 
-            return default;
+            return -1;
         }
         
         public void Add(TItem item)
         {
+            var elementCount = Count();
+            if (capacity == elementCount)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
             var newListElement = new MyListElement<TItem>(item);
 
-            if (null == Start)
+            if (null == start)
             {
-                Start = newListElement;
-                End = newListElement;
+                start = newListElement;
+                end = newListElement;
             }
             else
             {
-                var oldEnd = End;
+                var oldEnd = end;
 
                 oldEnd.Next = newListElement;
 
-                End = newListElement;
-                End.Previous = oldEnd;
+                end = newListElement;
+                end.Previous = oldEnd;
             }
         }
         
         public void AddAt(int index, TItem item)
         {
-            var tempListElement = Start;
-            var startIndex = 0;
+            var elementCount = Count();
+            if (capacity == elementCount)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var tempListElement = start;
+            var elementIndex = 0;
 
             while (null != tempListElement)
             {
-                if (startIndex == index)
+                if (elementIndex == index)
                 {
                     var newListElement = new MyListElement<TItem>(item);
-                    var previousCurrent = tempListElement.Previous;
+                    var tempListElementPrevious = tempListElement.Previous;
 
-                    previousCurrent.Next = newListElement;
-                    newListElement.Previous = previousCurrent;
+                    tempListElementPrevious.Next = newListElement;
+                    newListElement.Previous = tempListElementPrevious;
                     newListElement.Next = tempListElement;
                     tempListElement.Previous = newListElement;
                     return;
                 }
+
                 tempListElement = tempListElement.Next;
-                startIndex++;
+                elementIndex++;
             }
 
-            if (startIndex == index)
+            if (elementIndex == index)
             {
                 Add(item);
                 return;
             }
 
-            throw new ArgumentOutOfRangeException();
+            throw new IndexOutOfRangeException();
         }
         
-        public void DeleteAt(int i)
+        public void DeleteAt(int index)
         {
-            throw new NotImplementedException();
+            var listElementToBeDeleted = GetListElementByIndex(index);
+
+            if (null == listElementToBeDeleted)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var previous = listElementToBeDeleted.Previous;
+            var next = listElementToBeDeleted.Next;
+            previous.Next = next;
+            next.Previous = previous;
         }
-        
+
         public void Delete(TItem item)
         {
-            throw new NotImplementedException();
+            var tempListElement = start;
+
+            while (null != tempListElement)
+            {
+                if (item.Equals(tempListElement.Value))
+                {
+                    var previous = tempListElement.Previous;
+                    var next = tempListElement.Next;
+                    previous.Next = next;
+                    next.Previous = previous;
+                    return;
+                }
+
+                tempListElement = tempListElement.Next;
+            }
+
+            throw new InvalidOperationException();
         }
-        
+
         public void DeleteList()
         {
-            Start = null;
-            End = null;
+            start = null;
+            end = null;
         }
         
         public void Move(TItem item, int toIndex)
         {
-            throw new NotImplementedException();
+            var indexItem = Search(item);
+
+            Move(indexItem, toIndex);
         }
+
         public void Move(int fromIndex, int toIndex)
         {
-            throw new NotImplementedException();
+            var elementToMove = GetListElementByIndex(fromIndex);
+            if (null == elementToMove)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var tempListElement = start;
+            var elementIndex = 0;
+            var elementToMoveNext = elementToMove.Next;
+            var elementToMovePrevious = elementToMove.Previous;
+
+            while (null != tempListElement)
+            {
+                if (elementIndex == toIndex)
+                {
+                    if (fromIndex > toIndex)
+                    {
+                        var tempListElementPrevious = tempListElement.Previous;
+
+                        tempListElementPrevious.Next = elementToMove;
+                        elementToMove.Previous = tempListElementPrevious;
+                        elementToMove.Next = tempListElement;
+                        tempListElement.Previous = elementToMove;
+                        break;
+                    }
+
+                    var tempListElementNext = tempListElement.Next;
+
+                    tempListElementNext.Previous = elementToMove;
+                    elementToMove.Next = tempListElementNext;
+                    elementToMove.Previous = tempListElement;
+                    tempListElement.Next = elementToMove;
+                    break;
+                }
+
+                tempListElement = tempListElement.Next;
+                elementIndex++;
+            }
+
+            if (null == elementToMovePrevious)
+            {
+                start = elementToMoveNext;
+            }
+            else
+            {
+                elementToMovePrevious.Next = elementToMoveNext;
+            }
+            
+            if (null == elementToMoveNext)
+            {
+                end = elementToMovePrevious;
+            }
+            else
+            {
+                elementToMoveNext.Previous = elementToMovePrevious;
+            }
         }
         
         public void Swap(TItem itemOne, TItem itemTwo)
         {
-            throw new NotImplementedException();
+            var indexFirstItem = Search(itemOne);
+            var indexSecondItem = Search(itemTwo);
+
+            Swap(indexFirstItem, indexSecondItem);
         }
         
         public void Swap(int indexOne, int indexTwo)
         {
-            throw new NotImplementedException();
+            var listElementOne = GetListElementByIndex(indexOne);
+            var listElementTwo = GetListElementByIndex(indexTwo);
+
+            if (null == listElementOne || null == listElementTwo)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var listElementOneNext = listElementOne.Next;
+            var listElementOnePrevious = listElementOne.Previous;
+            var listElementTwoNext = listElementTwo.Next;
+            var listElementTwoPrevious = listElementTwo.Previous;
+
+            listElementOne.Next = listElementTwoNext;
+            listElementTwo.Next = listElementOneNext;
+            listElementOne.Previous = listElementTwoPrevious;
+            listElementTwo.Previous = listElementOnePrevious;
+
+            listElementOnePrevious.Next = listElementTwo;
+            listElementTwoPrevious.Next = listElementOne;
+            listElementOneNext.Previous = listElementTwo;
+            listElementTwoNext.Previous = listElementOne;
+        }
+
+        public int Count()
+        {
+            var tempListElement = start;
+            var elementIndex = 0;
+
+            while (null != tempListElement)
+            {
+                tempListElement = tempListElement.Next;
+                elementIndex++;
+            }
+
+            return elementIndex;
+        }
+
+        private MyListElement<TItem> GetListElementByIndex(int index)
+        {
+            var tempListElement = start;
+            var elementIndex = 0;
+
+            while (null != tempListElement)
+            {
+                if (elementIndex == index)
+                {
+                    return tempListElement;
+                }
+                tempListElement = tempListElement.Next;
+                elementIndex++;
+            }
+
+            return null;
         }
     }
 }
