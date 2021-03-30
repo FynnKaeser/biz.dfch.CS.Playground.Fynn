648e92fb00dd09cc912b2ce12ca7e906ece711e7
FynnKaeser <fynn.kaeser@icloud.com>
2021-03-30 11:55:24 +0200

    Implementing Test cases for all Queue methods #11

---
+++ src/biz.dfch.CS.Playground.Fynn.Tests/20210330/MyQueueTest.cs
@@ -0,0 +1,199 @@
+﻿/**
+ * Copyright 2021 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using biz.dfch.CS.Playground.Fynn._20210330;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.Playground.Fynn.Tests._20210330
+{
+    [TestClass]
+    public class MyQueueTest
+    {
+        [TestMethod]
+        public void ClearQueueSucceeds()
+        {
+            // Arrange
+            var sut = new MyQueue<int>(1);
+            sut.Enqueue(1);
+
+            var expectedCount = 0;
+
+            // Act
+            sut.Clear();
+            var resultCount = sut.Count;
+
+            // Assert
+            Assert.AreEqual(expectedCount, resultCount);
+        }
+        
+        [TestMethod]
+        public void DequeueEntryFromQueueSucceeds()
+        {
+            // Arrange
+            var sut = new MyQueue<int>(2);
+            sut.Enqueue(42);
+            sut.Enqueue(128);
+
+            var expectedCount = 1;
+            var expectedResult = 42;
+
+            // Act
+            var result = sut.Dequeue();
+            var resultCount = sut.Count;
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(expectedCount, resultCount);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void DequeueEntryFromEmptyQueueThrowsInvalidOperationException()
+        {
+            // Arrange
+            var sut = new MyQueue<int>(1);
+
+            // Act
+            var result = sut.Dequeue();
+
+            // Assert
+        }
+        
+        [TestMethod]
+        public void EnqueueEntryToQueueSucceeds()
+        {
+            // Arrange
+            var sut = new MyQueue<int>(1);
+            sut.Enqueue(42);
+
+            var expectedCount = 1;
+            var expectedResult = 42;
+
+            // Act
+            var result = sut.Peek();
+            var resultCount = sut.Count;
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(expectedCount, resultCount);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void EnqueueNullEntryToQueueThrowsArgumentNullException()
+        {
+            // Arrange
+            var sut = new MyQueue<string>(1);
+
+            // Act
+            sut.Enqueue(null);
+
+            // Assert
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void EnqueueSecondEntryToQueueWithCapacitySetToOneThrowsArgumentOutOfRangeException()
+        {
+            // Arrange
+            var sut = new MyQueue<int>(1);
+            sut.Enqueue(42);
+
+            // Act
+            sut.Enqueue(10);
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void PeekEntryFromQueueSucceeds()
+        {
+            // Arrange
+            var sut = new MyQueue<int>(2);
+            sut.Enqueue(42);
+            sut.Enqueue(128);
+
+            var expectedCount = 2;
+            var expectedResult = 42;
+
+            // Act
+            var result = sut.Peek();
+            var resultCount = sut.Count;
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(expectedCount, resultCount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void PeekEntryFromEmptyQueueThrowsInvalidOperationException()
+        {
+            // Arrange
+            var sut = new MyQueue<int>(1);
+
+            // Act
+            var result = sut.Peek();
+
+            // Assert
+        }
+        
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(-42)]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void SetInvalidCapacityForQueueThrowsArgumentOutOfRangeException(int capacity)
+        {
+            // Arrange
+            var sut = new MyQueue<int>(capacity);
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void ContainEntryInQueueReturnsTrue()
+        {
+            // Arrange
+            var sut = new MyQueue<int>(2);
+            sut.Enqueue(42);
+            sut.Enqueue(128);
+
+            // Act
+            var result = sut.Contains(42);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+        
+        [TestMethod]
+        public void NotContainingEntryInQueueReturnsFalse()
+        {
+            // Arrange
+            var sut = new MyQueue<int>(2);
+            sut.Enqueue(42);
+            sut.Enqueue(128);
+
+            // Act
+            var result = sut.Contains(100);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ src/biz.dfch.CS.Playground.Fynn/20210330/MyQueue.cs
@@ -0,0 +1,56 @@
+﻿/**
+ * Copyright 2021 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+
+namespace biz.dfch.CS.Playground.Fynn._20210330
+{
+    public class MyQueue<TValue>
+    {
+        public int Count { get; private set; }
+        private int capacity;
+
+        public MyQueue(int capacity)
+        {
+            this.capacity = capacity;
+        }
+
+        public void Enqueue(TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TValue Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TValue Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(TValue value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
