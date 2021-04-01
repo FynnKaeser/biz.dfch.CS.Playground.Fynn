3e11b4b311c85e6d205704deb3246f8fdc649191
FynnKaeser <fynn.kaeser@icloud.com>
2021-04-01 13:50:25 +0200

    MultiThreading tests

---
+++ src/biz.dfch.CS.Playground.Fynn.Tests/20210401/MultiThreadingTest.cs
@@ -0,0 +1,89 @@
+﻿/**
+ * Copyright 2021 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Threading;
+using biz.dfch.CS.Playground.Fynn._20210401;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.Playground.Fynn.Tests._20210401
+{
+    [TestClass]
+    public class MultiThreadingTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void CallingUnsafeDivideMethodWithTwoThreadsThrowsDividedByZeroException()
+        {
+            // Arrange
+            var sut = new MultiThreading();
+            var thread = new Thread(sut.UnsafeDivide);
+
+            // Act
+            thread.Start();
+            sut.UnsafeDivide();
+
+            // Assert
+        }
+        
+        [TestMethod]
+        public void CallingSafeDivideMethodWithTwoThreadsSucceeds()
+        {
+            // Arrange
+            var sut = new MultiThreading();
+            var thread = new Thread(sut.SafeDivide);
+
+            // Act
+            thread.Start();
+            sut.SafeDivide();
+
+            // Assert
+        }
+        
+        [TestMethod]
+        public void CallingFunctionsSynchronously()
+        {
+            // Arrange
+            var sut = new MultiThreading();
+
+            // Act
+            sut.Function1();
+            sut.Function2();
+
+            // Assert
+        }
+        
+        [TestMethod]
+        public void CallingFunctionsInMultiThreadedWay()
+        {
+            // Arrange
+            var sut = new MultiThreading();
+            var thread1 = new Thread(sut.Function1);
+            var thread2 = new Thread(sut.Function2);
+
+            // Act
+            thread1.Start();
+            thread2.Start();
+
+            while (thread2.IsAlive)
+            {
+                // Wait For Threads to finish
+            }
+
+            // Assert
+        }
+    }
+}
---
+++ src/biz.dfch.CS.Playground.Fynn/20210401/MultiThreading.cs
@@ -0,0 +1,81 @@
+﻿/**
+ * Copyright 2021 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace biz.dfch.CS.Playground.Fynn._20210401
+{
+    public class MultiThreading
+    {
+        private readonly Random random;
+        private int number1;
+        private int number2;
+
+        public MultiThreading()
+        {
+            random = new Random();
+        }
+
+        public void UnsafeDivide()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                number1 = random.Next(1, 2);
+                number2 = random.Next(1, 2);
+
+                var result= number1 / number2;
+                
+                number1 = 0;
+                number2 = 0;
+            }
+        }
+        
+        public void SafeDivide()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                lock (this)
+                {
+                    number1 = random.Next(1, 2);
+                    number2 = random.Next(1, 2);
+
+                    var result = number1 / number2;
+
+                    number1 = 0;
+                    number2 = 0;
+                }
+            }
+        }
+
+        public void Function1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Trace.WriteLine($"{nameof(Function1)}: {i}");
+            }
+        }
+        
+        public void Function2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Trace.WriteLine($"{nameof(Function2)}: {i}");
+            }
+        }
+    }
+}
