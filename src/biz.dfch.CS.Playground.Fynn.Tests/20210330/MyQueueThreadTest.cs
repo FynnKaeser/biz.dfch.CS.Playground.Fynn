56796295fa60821e163f1a7d492c07b946398d52
FynnKaeser <fynn.kaeser@icloud.com>
2021-04-07 13:54:39 +0200

    Thread Safety tests implementeation for queue #7

--- src/biz.dfch.CS.Playground.Fynn.Tests/20210330/MyQueueThreadTest.cs
+++ src/biz.dfch.CS.Playground.Fynn.Tests/20210330/MyQueueThreadTest.cs
@@ -24,7 +24,7 @@ namespace biz.dfch.CS.Playground.Fynn.Tests._20210330
     public class MyQueueThreadTest
     {
         [TestMethod]
-        public void CallingClearOnNewThreadWhileLookingForEntryLooksClearMethod()
+        public void ClearOnNewThreadWhileLookingForEntryLooksContainsMethod()
         {
             // Arrange
             var capacity = 1000000;
@@ -52,6 +52,127 @@ namespace biz.dfch.CS.Playground.Fynn.Tests._20210330
 
             // Assert
             Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ClearOnNewThreadWhilePeekForEntryLooksPeekMethod()
+        {
+            // Arrange;
+            var sut = new MyQueue<int>(1);
+            var arbitraryElement = 42;
+            var result = 0;
+
+            sut.Enqueue(arbitraryElement);
+
+            // Act 
+            var peekThread = new Thread(() => result = sut.Peek());
+            peekThread.Start();
+
+            var clearThread = new Thread(sut.Clear);
+            clearThread.Start();
+
+            while (peekThread.IsAlive || clearThread.IsAlive)
+            {
+                // Wait for threads to complete
+            }
+
+            // Assert
+            Assert.AreEqual(arbitraryElement, result);
+        }
+
+        [TestMethod]
+        public void DequeueEntryOnNewThreadWhilePeekForEntryLooksPeekMethod()
+        {
+            // Arrange;
+            var sut = new MyQueue<int>(3);
+            var arbitraryElement = 42;
+            var secondArbitraryElement = 142;
+            var resultPeek = 0;
+            var resultDequeue = 0;
+
+            sut.Enqueue(arbitraryElement);
+            sut.Enqueue(secondArbitraryElement);
+            sut.Enqueue(secondArbitraryElement);
+
+            // Act 
+            var peekThread = new Thread(() => resultPeek = sut.Peek());
+            peekThread.Start();
+
+            var dequeueThread = new Thread(() => resultDequeue = sut.Dequeue());
+            dequeueThread.Start();
+
+            while (peekThread.IsAlive || dequeueThread.IsAlive)
+            {
+                // Wait for threads to complete
+            }
+
+            // Assert
+            Assert.AreEqual(arbitraryElement, resultPeek);
+            Assert.AreEqual(arbitraryElement, resultDequeue);
+        }
+
+        [TestMethod]
+        public void PeekEntryOnNewThreadWhileDequeueEntryLooksDequeueMethod()
+        {
+            // Arrange;
+            var sut = new MyQueue<int>(3);
+            var arbitraryElement = 42;
+            var secondArbitraryElement = 142;
+            var resultPeek = 0;
+            var resultDequeue = 0;
+
+            sut.Enqueue(arbitraryElement);
+            sut.Enqueue(secondArbitraryElement);
+            sut.Enqueue(secondArbitraryElement);
+
+            // Act
+            var dequeueThread = new Thread(() => resultDequeue = sut.Dequeue());
+            dequeueThread.Start();
+
+            var peekThread = new Thread(() => resultPeek = sut.Peek());
+            peekThread.Start();
+
+            while (peekThread.IsAlive || dequeueThread.IsAlive)
+            {
+                // Wait for threads to complete
+            }
+
+            // Assert
+            Assert.AreEqual(arbitraryElement, resultDequeue);
+            Assert.AreEqual(secondArbitraryElement, resultPeek);
+        }
+
+        [TestMethod]
+        public void DequeueEntryOnNewThreadWhileLookingForEntryLooksContainsMethod()
+        {
+            // Arrange
+            var capacity = 1000000;
+            var result = false;
+            var resultDequeue = 0;
+            var sut = new MyQueue<int>(capacity);
+
+            for (var i = 0; i < capacity; i++)
+            {
+                sut.Enqueue(i);
+            }
+
+            var firstEntry = 0;
+
+            // Act 
+            var containsThread = new Thread(() => result = sut.Contains(firstEntry));
+            containsThread.Start();
+
+            var clearThread = new Thread(() => resultDequeue = sut.Dequeue());
+            clearThread.Start();
+
+            while (containsThread.IsAlive || clearThread.IsAlive)
+            {
+                // Wait for threads to complete
+            }
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.AreEqual(firstEntry, resultDequeue);
         }
     }
 }
