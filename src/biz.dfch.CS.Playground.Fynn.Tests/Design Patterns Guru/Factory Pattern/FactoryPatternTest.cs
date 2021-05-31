79b96e88b8737552b28578aa07f80253fb33d723
FynnKaeser <fynn.kaeser@icloud.com>
2021-05-31 15:26:54 +0200

    Implementation of Factory Pattern Tests #13

---
+++ src/biz.dfch.CS.Playground.Fynn.Tests/Design Patterns Guru/Factory Pattern/FactoryPatternTest.cs
@@ -0,0 +1,53 @@
+﻿/**
+ * Copyright 2021 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using biz.dfch.CS.Playground.Fynn.Design_Patterns_Guru.Factory_Pattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.Playground.Fynn.Tests.Design_Patterns_Guru.Factory_Pattern
+{
+    [TestClass]
+    public class FactoryPatternTest
+    {
+        [TestMethod]
+        public void StartingSourceDeviceReturnsFalse()
+        {
+            // Arrange
+            Creator sut = new SourceCreator();
+            var expectedResult = false;
+
+            // Act
+            var result = sut.Start();
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+        
+        [TestMethod]
+        public void StartingTargetDeviceReturnsFalse()
+        {
+            // Arrange
+            Creator sut = new TargetCreator();
+            var expectedResult = true;
+
+            // Act
+            var result = sut.Start();
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
