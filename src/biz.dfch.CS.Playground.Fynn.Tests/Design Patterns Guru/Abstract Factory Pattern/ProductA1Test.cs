7332b813f4d32c65f69f07d50d673534b52dacba
FynnKaeser <fynn.kaeser@icloud.com>
2021-06-03 11:20:20 +0200

    Test and Equals/GetHashCode Implementation for Product Model Classes #13

---
+++ src/biz.dfch.CS.Playground.Fynn.Tests/Design Patterns Guru/Abstract Factory Pattern/ProductA1Test.cs
@@ -0,0 +1,57 @@
+﻿/**
+ * Copyright 2021 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using biz.dfch.CS.Playground.Fynn.Design_Patterns_Guru.Abstract_Factory_Pattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.Playground.Fynn.Tests.Design_Patterns_Guru.Abstract_Factory_Pattern
+{
+    [TestClass]
+    public class ProductA1Test
+    {
+        [TestMethod]
+        public void EqualsMethodReturnsExpectedResult()
+        {
+            // Arrange
+            var arbitraryName = "arbitraryName";
+            var arbitraryIpAddress = "1.2.3.4";
+            var sut = new ProductA1(arbitraryName, arbitraryIpAddress);
+            var otherSut = new ProductA1(arbitraryName, arbitraryIpAddress);
+
+            // Act
+            var result = sut.Equals(otherSut);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void GetHashCodeMethodReturnsExpectedResult()
+        {
+            // Arrange
+            var arbitraryName = "arbitraryName";
+            var arbitraryIpAddress = "1.2.3.4";
+            var sut = new ProductA1(arbitraryName, arbitraryIpAddress);
+            var expectedResult = 6363335;
+
+            // Act
+            var result = sut.GetHashCode();
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
--- src/biz.dfch.CS.Playground.Fynn/Design Patterns Guru/Abstract Factory Pattern/ProductA2.cs
+++ src/biz.dfch.CS.Playground.Fynn/Design Patterns Guru/Abstract Factory Pattern/ProductA2.cs
@@ -31,5 +31,27 @@ namespace biz.dfch.CS.Playground.Fynn.Design_Patterns_Guru.Abstract_Factory_Patt
         {
             throw new System.NotImplementedException();
         }
+
+        private bool Equals(ProductA2 other)
+        {
+            return Name == null ? Name == other.Name : Name.Equals(other.Name) && IpAddress == null ? IpAddress == other.IpAddress : IpAddress.Equals(other.IpAddress);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+
+            return Equals((ProductA2)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashName = Name == null ? 0 : Name.GetHashCode();
+            var hashIpAddress = IpAddress == null ? 0 : IpAddress.GetHashCode();
+
+            return hashName ^ hashIpAddress;
+        }
     }
 }
