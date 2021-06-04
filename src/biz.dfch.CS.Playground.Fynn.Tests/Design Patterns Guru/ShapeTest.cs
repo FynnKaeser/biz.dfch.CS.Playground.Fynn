f2cf42ddca696a95a9a961c8cb9d0a31160b5ee2
FynnKaeser <fynn.kaeser@icloud.com>
2021-06-04 12:02:27 +0200

    Prototype Pattern Implementation - Test Implementation - ICloneable<T> implementation - LengthUnit for more exact dimension definition #13

---
+++ src/biz.dfch.CS.Playground.Fynn.Tests/Design Patterns Guru/ShapeTest.cs
@@ -0,0 +1,116 @@
+﻿/**
+ * Copyright 2021 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using biz.dfch.CS.Playground.Fynn.Design_Patterns_Guru.Prototype_Pattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.Playground.Fynn.Tests.Design_Patterns_Guru
+{
+    [TestClass]
+    public class ShapeTest
+    {
+        [TestMethod]
+        public void CloneShapeSucceeds()
+        {
+            // Arrange
+            var sut = new Shape
+            {
+                Color = "Brown",
+                Height = 120,
+                Length = 42,
+                Width = 60,
+                LengthUnit = LengthUnit.Centimeter
+            };
+
+            // Act
+            var clone = sut.Clone();
+
+            // Assert
+            Assert.AreEqual(sut, clone);
+        }
+
+        [TestMethod]
+        public void EqualsSucceeds()
+        {
+            // Arrange
+            var sut = new Shape
+            {
+                Color = "Brown",
+                Height = 120,
+                Length = 42,
+                Width = 60,
+                LengthUnit = LengthUnit.Centimeter
+            };
+
+            var referenceShape = new Shape
+            {
+                Color = "Brown",
+                Height = 120,
+                Length = 42,
+                Width = 60,
+                LengthUnit = LengthUnit.Centimeter
+            };
+            
+            // Act
+            var result = sut.Equals(referenceShape);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+        
+        [TestMethod]
+        public void CallEqualsWithOtherBeingNullReturnsFalse()
+        {
+            // Arrange
+            var sut = new Shape
+            {
+                Color = "Brown",
+                Height = 120,
+                Length = 42,
+                Width = 60,
+                LengthUnit = LengthUnit.Centimeter
+            };
+            
+            // Act
+            var result = sut.Equals(null);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void GetHashCodeSucceeds()
+        {
+            // Arrange
+            var sut = new Shape
+            {
+                Color = "Brown",
+                Height = 120,
+                Length = 42,
+                Width = 60,
+                LengthUnit = LengthUnit.Centimeter
+            };
+            var expectedHashCode = 1091142634;
+
+            // Act
+            var result = sut.GetHashCode();
+
+            // Assert
+            Assert.AreEqual(expectedHashCode, result);
+        }
+
+    }
+}
---
+++ src/biz.dfch.CS.Playground.Fynn/Design Patterns Guru/Prototype Pattern/Shape.cs
@@ -0,0 +1,64 @@
+﻿/**
+ * Copyright 2021 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+namespace biz.dfch.CS.Playground.Fynn.Design_Patterns_Guru.Prototype_Pattern
+{
+    public class Shape : ICloneable<Shape>
+    {
+        public string Color { get; set; }
+        public int Width { get; set; }
+        public int Length { get; set; }
+        public int Height { get; set; }
+        public LengthUnit LengthUnit { get; set; } = LengthUnit.None;
+        
+        public Shape Clone()
+        {
+            return (Shape) MemberwiseClone();
+        }
+
+        private bool Equals(Shape other)
+        {
+            if (null == other) return false;
+
+            return Color == null ? Color == other.Color : Color.Equals(other.Color)
+                                                          && Width.Equals(other.Width)
+                                                          && Length.Equals(other.Length)
+                                                          && Height.Equals(other.Height)
+                                                          && LengthUnit.Equals(other.LengthUnit);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+
+            return Equals((Shape)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashColor = Color == null ? 0 : Color.GetHashCode();
+            var hashLength = Length.GetHashCode();
+            var hashWidth = Width.GetHashCode();
+            var hashHeight = Height.GetHashCode();
+            var hashLengthUnit = LengthUnit.GetHashCode();
+
+            return hashColor ^ hashLength ^ hashWidth ^ hashHeight ^ hashLengthUnit;
+        }
+    }
+}
