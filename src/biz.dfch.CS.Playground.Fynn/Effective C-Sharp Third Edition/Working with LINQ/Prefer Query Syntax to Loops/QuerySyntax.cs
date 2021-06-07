680c80fb720fb0ee1a95996c725335b5391c11bd
FynnKaeser <fynn.kaeser@icloud.com>
2021-06-07 15:03:05 +0200

    Ordering comparision for loop and query #14

--- src/biz.dfch.CS.Playground.Fynn/Effective C-Sharp Third Edition/Working with LINQ/Prefer Query Syntax to Loops/QuerySyntax.cs
+++ src/biz.dfch.CS.Playground.Fynn/Effective C-Sharp Third Edition/Working with LINQ/Prefer Query Syntax to Loops/QuerySyntax.cs
@@ -18,6 +18,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace biz.dfch.CS.Playground.Fynn.Working_with_LINQ.Prefer_Query_Syntax_to_Loops
 {
@@ -84,6 +85,36 @@ namespace biz.dfch.CS.Playground.Fynn.Working_with_LINQ.Prefer_Query_Syntax_to_L
             return from x in Enumerable.Range(0, 100)
                 from y in Enumerable.Range(0, 100)
                 where x + y < 100
+                select Tuple.Create(x, y);
+        }
+
+        public IEnumerable<Tuple<int, int>> CreateOrderedValuePairForLoop()
+        {
+            var storage = new List<Tuple<int, int>>();
+            for (int x = 0; x < 100; x++)
+            {
+                for (int y = 0; y < 100; y++)
+                {
+                    if (x + y < 100)
+                    {
+                        storage.Add(Tuple.Create(x, y));
+                    }
+                }
+            }
+
+            storage.Sort((point1, point2)
+                => (point2.Item1 * point2.Item1 + point2.Item2 * point2.Item2)
+                .CompareTo(point1.Item1 * point1.Item1 + point1.Item2 * point1.Item2));
+
+            return storage;
+        }
+
+        public IEnumerable<Tuple<int, int>> CreateOrderValuePairQuery()
+        {
+            return from x in Enumerable.Range(0, 100)
+                from y in Enumerable.Range(0, 100)
+                where x + y < 100
+                orderby (x*x + y*y) descending 
                 select Tuple.Create(x, y);
         }
     }
