303acee15db3697ea45263f17598cdcc08d2db7f
FynnKaeser <fynn.kaeser@icloud.com>
2021-06-08 14:52:36 +0200

    Device Test implementation #13

--- src/biz.dfch.CS.Playground.Fynn.Tests/Design Patterns Guru/Bridge Pattern/DestinationDeviceTest.cs
+++ src/biz.dfch.CS.Playground.Fynn.Tests/Design Patterns Guru/Bridge Pattern/DestinationDeviceTest.cs
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+using System;
+using biz.dfch.CS.Playground.Fynn.Design_Patterns_Guru.Bridge_Pattern;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace biz.dfch.CS.Playground.Fynn.Tests.Design_Patterns_Guru.Bridge_Pattern
@@ -21,5 +23,41 @@ namespace biz.dfch.CS.Playground.Fynn.Tests.Design_Patterns_Guru.Bridge_Pattern
     [TestClass]
     public class DestinationDeviceTest
     {
+        [TestMethod]
+        public void SetIpAddressSucceeds()
+        {
+            // Arrange
+            var sut = new DestinationDevice();
+            var arbitraryIpAddress = new IpAddress(1, 2, 3, 4);
+
+            var expectedSection1 = 1;
+            var expectedSection2 = 2;
+            var expectedSection3 = 3;
+            var expectedSection4 = 4;
+
+            // Act
+            sut.SetIpAddress(arbitraryIpAddress);
+
+            // Assert
+            var result = sut.IpAddress;
+            
+            Assert.AreEqual(expectedSection1, result.Section1);
+            Assert.AreEqual(expectedSection2, result.Section2);
+            Assert.AreEqual(expectedSection3, result.Section3);
+            Assert.AreEqual(expectedSection4, result.Section4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SetIpAddressToNullObjectThrowsArgumentNullException()
+        {
+            // Arrange
+            var sut = new DestinationDevice();
+
+            // Act
+            sut.SetIpAddress(null);
+
+            // Assert
+        }
     }
 }
