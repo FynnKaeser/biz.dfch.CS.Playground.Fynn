335ec6bd556c7c066d556eda5d6ed2ce618ebf89
FynnKaeser <fynn.kaeser@icloud.com>
2021-06-16 09:41:33 +0200

    ShortcutTest implementation - OnSave and OnCopy test implementation #13

--- src/biz.dfch.CS.Playground.Fynn.Tests/Design Patterns Guru/Command Pattern/ShortcutTest.cs
+++ src/biz.dfch.CS.Playground.Fynn.Tests/Design Patterns Guru/Command Pattern/ShortcutTest.cs
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+using biz.dfch.CS.Playground.Fynn.Design_Patterns_Guru.Command_Pattern;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace biz.dfch.CS.Playground.Fynn.Tests.Design_Patterns_Guru.Command_Pattern
@@ -21,5 +22,32 @@ namespace biz.dfch.CS.Playground.Fynn.Tests.Design_Patterns_Guru.Command_Pattern
     [TestClass]
     public class ShortcutTest
     {
+        [TestMethod]
+        public void OnSaveSucceeds()
+        {
+            // Arrange
+            var sut = new Shortcut();
+            var expectedResult = "Has Been Saved!";
+
+            // Act
+            var result = sut.OnSave();
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void OnCopySucceeds()
+        {
+            // Arrange
+            var sut = new Shortcut();
+            var expectedResult = "Has Been Saved!";
+
+            // Act
+            var result = sut.OnCopy();
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
