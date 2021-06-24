b8c7457839ad2e8d6345df0580c2e1116a6fb4be
FynnKaeser <fynn.kaeser@icloud.com>
2021-06-24 11:23:53 +0200

    Start Strategy Pattern - Implementation of IRoutPlaner interface - Implementation of Place and Route class - Implementation of PlaceTest and RouteTest

---
+++ src/biz.dfch.CS.Playground.Fynn.Tests/Design Patterns Guru/Strategy Pattern/RouteTest.cs
@@ -0,0 +1,54 @@
+﻿/**
+ * Copyright 2021 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using biz.dfch.CS.Playground.Fynn.Design_Patterns_Guru.Strategy_Pattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.Playground.Fynn.Tests.Design_Patterns_Guru.Strategy_Pattern
+{
+    [TestClass]
+    public class RouteTest
+    {
+        [TestMethod]
+        public void InitializeRouteSucceeds()
+        {
+            // Arrange
+            var arbitraryTime = "2h 42min";
+
+            // Act
+            var sut = new Route(arbitraryTime);
+
+            // Assert
+            var result = sut.Time;
+            Assert.AreEqual(arbitraryTime, result);
+        }
+
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow(" ")]
+        [DataRow(null)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InitializeInvalidTimeThrowsArgumentException(string invalidTime)
+        {
+            // Arrange
+            // Act
+            var sut = new Route(invalidTime);
+
+            // Assert
+        }
+    }
+}
---
+++ src/biz.dfch.CS.Playground.Fynn/Design Patterns Guru/Strategy Pattern/Place.cs
@@ -0,0 +1,46 @@
+﻿/**
+ * Copyright 2021 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+
+namespace biz.dfch.CS.Playground.Fynn.Design_Patterns_Guru.Strategy_Pattern
+{
+    public class Place
+    {
+        private readonly int LongitudeMax = 180;
+        private readonly int LatitudeMax = 90;
+        private readonly int CoordinateMin = 0;
+
+        public int Longitude { get; set; }
+        public int Latitude { get; set; }
+
+        public Place(int longitude, int latitude)
+        {
+            if (longitude > LongitudeMax || longitude < CoordinateMin)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            
+            if (latitude > LatitudeMax || latitude < CoordinateMin)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            Longitude = longitude;
+            Latitude = latitude;
+        }
+    }
+}
