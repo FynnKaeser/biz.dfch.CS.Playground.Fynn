c1a23806c98047a0bf1c7b8c7b018dbcd8e7bc3b
FynnKaeser <fynn.kaeser@icloud.com>
2021-06-24 16:18:04 +0200

    Example of Fluent interface #13

---
+++ src/biz.dfch.CS.Playground.Fynn/Fluent Interface/SqlDatabaseManager.cs
@@ -0,0 +1,50 @@
+﻿/**
+ * Copyright 2021 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+namespace biz.dfch.CS.Playground.Fynn.Fluent_Interface
+{
+    public class SqlDatabaseManager
+    {
+        public SqlDatabaseManager Connect(string connectionString)
+        {
+            // Connect to the Database
+
+            return this;
+        }
+
+        public SqlDatabaseManager SetPolicy()
+        {
+            // Set the Policies for the Database
+
+            return this;
+        }
+
+        public SqlDatabaseManager MakeBackup()
+        {
+            // Make a Backup of the current Database
+
+            return this;
+        }
+
+        public SqlDatabaseManager CloseConnection()
+        {
+            // Make a Backup of the current Database
+
+            return this;
+        }
+    }
+}
