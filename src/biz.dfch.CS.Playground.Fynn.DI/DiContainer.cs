358a17f27a515806927cea693e505acd50f42aab
FynnKaeser <fynn.kaeser@icloud.com>
2021-07-01 17:24:50 +0200

    Create Foo Model Structure with DiContainer as singleton - Added Default registry

---
+++ src/biz.dfch.CS.Playground.Fynn.DI/DiContainer.cs
@@ -0,0 +1,51 @@
+﻿/**
+ * Copyright 2021 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using StructureMap;
+
+namespace biz.dfch.CS.Playground.Fynn.DI
+{
+    public class DiContainer
+    {
+        private static readonly object _lock = new object();
+        private static DiContainer _diContainer;
+        public Container Container { get; }
+
+        private DiContainer()
+        {
+            var registry = new Registry();
+            registry.IncludeRegistry<DefaultRegistry>();
+
+            Container = new Container(registry);
+        }
+
+        public static DiContainer GetInstance()
+        {
+            if (null == _diContainer)
+            {
+                lock (_lock)
+                {
+                    if (null == _diContainer)
+                    {
+                        _diContainer = new DiContainer();
+                    }
+                }
+            }
+
+            return _diContainer;
+        }
+    }
+}
