9cab047345a3ad472ca00fda9239383ce1308c63
FynnKaeser <fynn.kaeser@icloud.com>
2021-07-01 14:08:09 +0200

    Create new Projects for implementing DI examples and Testing the DI

---
+++ src/biz.dfch.CS.Playground.Fynn.DI.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("biz.dfch.CS.Playground.Fynn.DI.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("biz.dfch.CS.Playground.Fynn.DI.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("362c6231-d22b-4fe0-b984-918a4b1a5d64")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
